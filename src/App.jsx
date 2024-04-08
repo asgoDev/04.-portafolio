cd9231201c3aee62a4e556e46c734120f7b864c5
asgoDev <asgodev@gmail.com>
2024-04-07 20:15:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./styles/app-container.css";
+
+function App() {
+  return (
+    <div className="app__container">
+      <header className="app__header">
+        Aquiles Gonzalez
+      </header>
+      <main className="app__main">
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
